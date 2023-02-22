844a25e8898eaeb756885e0945e8ebbe0bbf1475
Prayash Thapa <prayasht@gmail.com>
2023-02-22 00:19:17 -0800

    Initial Commit

---
+++ Libro/LibroApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibroApp.swift
+//  Libro
+//
+//  Created by Prayash Thapa on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LibroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
